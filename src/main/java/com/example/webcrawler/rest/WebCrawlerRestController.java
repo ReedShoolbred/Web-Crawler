5872d9d65f05aefc1a50e840d775a26b3bfcaf42
ReedShoolbred <reedshoolbred@gmail.com>
2025-01-08 14:40:32 -0500

    Added CORS support

--- src/main/java/com/example/webcrawler/rest/WebCrawlerRestController.java
+++ src/main/java/com/example/webcrawler/rest/WebCrawlerRestController.java
@@ -5,26 +5,22 @@ import com.example.webcrawler.webcrawler.WebCrawlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/url")
+@CrossOrigin(origins = "http://localhost:5500/")
+@RequestMapping("/api")
 public class WebCrawlerRestController {
 
     @Autowired
     WebCrawlerService webCrawlerService;
 
     //save a single URLEntitty
-    @PostMapping("/save")
-    public void save() {
-
-    }
-
-    //save a collection of URLEntity objects
-    @PostMapping("/saveall")
-    public void saveAll() {
-
+    @PostMapping("/crawl")
+    public ArrayList<URL> crawl(@RequestBody String url) {
+        return webCrawlerService.crawl(url);
     }
 
     //delete all elements from database and return number of items deleted
